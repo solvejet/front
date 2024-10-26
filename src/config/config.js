552a7210ece8aacd46e8777509a2abae4f6fc7a3
nikhil sacena <nikhilhill19@gmail.com>
2024-10-26 21:10:06 +0530

    BE prod URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 
 export const config = {
-  baseURL: "http://localhost:5000/api"
-  // baseURL: "https://api.pixe.in/api"
+  // baseURL: "http://localhost:5000/api"
+  baseURL: "https://api.pixe.in/api"
   // baseURL: process.env.REACT_APP_API_URL
 };
