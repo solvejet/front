6548070aa1e7594772b34da0a06b872441d04af8
nikhil sacena <nikhilhill19@gmail.com>
2024-11-12 22:47:22 +0530

    getColumn api change due to headers error

--- src/api/users/columns/getColumns.js
+++ src/api/users/columns/getColumns.js
@@ -1,15 +1,14 @@
-import { apiClient } from "../../axios"
+import { apiClient } from "../../axios";
 export const userColumns = async (token) => {
-    try {
-        const response = await apiClient.get("/user/schema", {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-                'Custom-Header': 'Custom-Value'
-            }
-        }); return { data: response.data, error: null };  // Return data and null error return response.data;  // Assuming you want to return the response data
-    } catch (error) {
-        console.error("Error fetching columns:", error);
-        return { data: null, error };  // Return null data and error
-    }
-}
+  try {
+    const response = await apiClient.get("/user/schema", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { data: response.data, error: null }; // Return data and null error return response.data;  // Assuming you want to return the response data
+  } catch (error) {
+    console.error("Error fetching columns:", error);
+    return { data: null, error }; // Return null data and error
+  }
+};
